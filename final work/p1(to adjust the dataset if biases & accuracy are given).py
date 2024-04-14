18d43f12aa60ffd13af937b115ab8051ee8f9997
pranavv19 <96830638+pranavv19@users.noreply.github.com>
2024-04-14 19:01:12 +0530

    Update p1(to adjust the dataset if biases & accuracy are given).py

--- final work/p1(to adjust the dataset if biases & accuracy are given).py
+++ final work/p1(to adjust the dataset if biases & accuracy are given).py
@@ -2,15 +2,12 @@ import pandas as pd
 import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
 
-# Load the historical sales data from the Excel file
 sales_file_path = 'dataraw.xlsx'
 dataraw_df = pd.read_excel(sales_file_path)
 
-# Extract the relevant data for modeling
 product_names = dataraw_df['Product Name '].values
 sales_data = dataraw_df.iloc[:, 3:].astype(float)
 
-# Load the demand forecast data from the Excel file
 demand_file_path = 'demandforecast.xlsx'
 demand_df = pd.read_excel(demand_file_path)
 
@@ -18,12 +15,10 @@ demand_df['Product Name '] = demand_df['Product Name '].astype(str).str.strip()
 
 sales_data = sales_data.ffill(axis=1)
 
-order = (1, 1, 1)  # You may need to tune these parameters based on the data
-
+order = (1, 1, 1)  
 for i, selected_product in enumerate(product_names):
     selected_sales = sales_data.loc[dataraw_df['Product Name '] == selected_product].values.flatten()
 
-    # Check if demand forecast data is available for the selected product
     demand_data = demand_df[demand_df['Product Name '] == selected_product]
     if not demand_data.empty:
         for quarter in ['2023q1', '2023q2', '2023q3']:
@@ -33,11 +28,9 @@ for i, selected_product in enumerate(product_names):
             accuracy = float(str(demand_data[accuracy_col].values[0]).rstrip('%')) / 100.0
             bias = float(str(demand_data[bias_col].values[0]).rstrip('%')) / 100.0
 
-            # Apply adjustments to the sales data for each quarter
             sales_data.loc[dataraw_df['Product Name '] == selected_product] *= (1 + bias)
             sales_data.loc[dataraw_df['Product Name '] == selected_product] *= (1 + accuracy)
 
-# Save the adjusted dataraw DataFrame to a new Excel file
 adjusted_sales_file_path = 'adjusted_dataraw.xlsx'
 sales_data.to_excel(adjusted_sales_file_path, index=False)
 print(f"Adjusted sales data saved to {adjusted_sales_file_path}")
